4d8bd5a4d3c15a4a9c8e480b1099ac85294ca23f
BokaiZhu <36682386+BokaiZhu@users.noreply.github.com>
2020-07-29 15:36:05 -0700

    Delete MibiPlotDataAndCap.m

--- code/Lib/MibiPlotDataAndCap.m
+++
@@ -1,9 +0,0 @@
-function MibiPlotDataAndCap(data,cap,titlestr)
-% function MibiPlotDataAndCap(data,cap)
-% function plots the data and sets any value larger than cap to cap
-
-currdata = data;
-currdata(currdata>cap) = cap;
-figure;
-imagesc(currdata);
-title(titlestr);
